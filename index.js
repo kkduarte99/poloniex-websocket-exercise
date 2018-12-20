0f99f0b6db12dc6f4e5788c88c584f529bf32d6a
kkduarte99 <kkduarte.kd@gmail.com>
2018-12-19 16:49:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const WebSocket = require("ws");
+const socket =require("socket.io");
+
+// create new websocket for poloniex
+const poloniexWS = new WebSocket("wss://api2.poloniex.com");
+
+//App init
+let application = express();
+let server = application.listen(4000, () => {
+  console.log("initializing requests from port 4000");
+});
+application.use(express.static("pub"));
+
+let convertedPrice = 0;
+let rawPrice = 0;
+let bitcoinTickerInfo = {};
+let priceUsd;
+
+
+let io = socket(server);
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
+
+  socket.on("bitcoin", (data) => {
+    //message the data
+    bitcoinTickerInfo = JSON.parse(data.bitcoinTickerInfo)[0];
+    priceUsd = parseFloat(bitcoinTickerInfo["price_usd"]);
+  });
+});
+
+poloniexWS.on("open", () => {
+  console.log("successfully connected to wss://api2.poloniex.com");
+
+  //since the initial message must be a string, we stringify the message to send
+  let subscriptionMessage = JSON.stringify({
+    "command": "subscribe",
+    "channel": 1002});
+
+  poloniexWS.send(subscriptionMessage);
+});
+
+//listen to all the necessary messages on the 1002 WS channel
+poloniexWS.on("message", (data) => {
+  if (data) {
+    //only continue if data is truthy
+    data = JSON.parse(data);
+    let messagedData = data[data.length - 1];
+    let lastValue = parseFloat(messagedData[messagedData.length - 1]);
+    if (lastValue && priceUsd) {
+      //only calculate if both are truthy
+      convertedPrice = calculateCurrentPrice(priceUsd, lastValue);
+      io.sockets.emit("currentPrices", {
+        convertedPrice: convertedPrice.toString(),
+        rawPrice: lastValue.toString()
+      });
+    }
+  }
+});
+
+function calculateCurrentPrice(bitcoinUsdPrice, value) {
+  return bitcoinUsdPrice * value;
+}
